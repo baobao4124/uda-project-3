bf7ff105a4fcecdd8997f323b3873409617a17a9
Oliver <bao_bao4124@yahoo.com>
2023-06-06 16:09:01 +0700

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=abcd12345
+export POSTGRES_HOST=database-2.coh0zfu7iheu.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=project-3-756704422180
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=abc
+export URL=http://localhost:8100
